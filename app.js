0dc79aadc812e035ab58ca7697cca8a23424f4aa
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-07-03 15:51:11 +0530

    added processing

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-var primeInput = document.querySelector("#btn-prime");
+// Input
+var primeInput = document.querySelector("#input-box");
+var btnPrime = document.querySelector("#btn-prime");
+var primeOutput = document.querySelector("#output-box");
+
+// inner text
+primeInput.innerText = "Enter your date of birth in (DD/MM) format";
+primeOutput.innerText= "Your result will be shown here";
 
 
 
-function clickHandler(){
-    console.log("clicked!")
+
+function clickHandler(input){
+    input = primeInput.value;
+    function checkPrimeYear(){
+        input = newDate()
+
+
+    }
+    
+    
+    console.log("clicked!");
+    console.log("input: ",input);
+    primeOutput.innerText = "Output: "+input;
+
+    
+    
 }
-primeInput.addEventListener("click",clickHandler)
+btnPrime.addEventListener("click",clickHandler)
